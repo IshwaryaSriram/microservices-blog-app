82f9bc2e7b9cd6dcb2cfb8d0673e43563f12718b
IshwaryaSriram <ishwaryasriram@live.com>
2023-09-20 16:14:34 +0530

    moderation service added

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+
+        switch(comment.status) {
+            case 'approved': {
+                content = comment.content;
+                break;
+            }
+
+            case 'pending': {
+                content = 'Pending moderation';
+                break;
+            }
+
+            case 'rejected': {
+                content = 'Rejected';
+                break;
+            }
+
+            default: {
+                content = 'Pending moderation';
+                break;
+            }
+        }
+        return <li key={comment.id}>{content}</li>
     });
 
     return <ul>
