720d33cd4d9e9107c279dc76e02506592a6ab4a2
IshwaryaSriram <ishwaryasriram@live.com>
2023-09-28 13:58:24 +0530

    added wsl version change script

---
+++ ubuntu-wsl-distro-change.ps1
@@ -0,0 +1,4 @@
+﻿Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+wsl --set-version Ubuntu 2
+wsl --list --verbose
